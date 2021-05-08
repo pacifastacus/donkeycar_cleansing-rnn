d3d1a2125b2ef8f9338d18ad0bfcf302a4e6950c
pacifastacus <palkovics.denes@gmail.com>
2021-05-08 13:47:17 +0200

    Commit

--- build_net.py
+++ build_net.py
@@ -10,7 +10,7 @@ import os
 import glob
 import json
 
-TEST_DATA_DIR = data_dir = "/media/users/DATA/Projektek/classify-donkey-dataset/test_data/img"
+TEST_DATA_DIR = data_dir = "/media/users/DATA/Projektek/donkeycar-data_cleansing/test_data/img"
 
 def load_image_subnet(file='rnn_cnn_top.h5'):
 	model = load_model(file, compile=False)
@@ -33,26 +33,66 @@ def get_img_flist_ordered(data_dir):
 	im_list.sort(key= lambda fname: int(os.path.basename(fname).split('_')[0]))
 	return im_list
 	
-def seq_generator(img_list,seq_len=9):
-	img_seq = []
-	# Preload img_seq
-	while len(img_seq) < seq_len:
-		im = get_img(img_list[0])
-		img_seq.append(im)
-	
-	# Yield the next seq_len-th image
-	for i in range(len(img_list)):
-		im = get_img(img_list[i])
-		img_seq = img_seq[1:]
-		img_seq.append(im)
-		yield np.array(img_seq).reshape(1,seq_len,*im.shape)
-	
-	
-def get_img(im):
-	with open(im) as f:
-		im_name = json.load(f)["cam/image_array"]
+# def seq_generator(data_dir, json_list, seq_len=9):
+# 	img_seq = []
+# 	ctrl_seq = []
+# 	# Preload img_seq
+# 	with open(json_list[0]) as f:
+# 		datas = json.load(f)
+# 	while len(img_seq) < seq_len:
+# 		im = get_img(data_dir,datas["cam/image_array"])
+# 		ctrl = datas["user/throttle"], datas["user/angle"]
+# 		img_seq.append(im)
+# 		ctrl_seq.append(ctrl)
+# 	# Yield the next seq_len-th image
+# 	for i in range(len(json_list)):
+# 		with open(json_list[i]) as f:
+# 			#im = get_img(json_list[i])
+# 			datas = json.load(f)
+# 			im = get_img(data_dir,datas["cam/image_array"])
+# 			ctrl = datas["user/throttle"], datas["user/angle"]
+# 			img_seq = img_seq[1:]
+# 			img_seq.append(im)
+# 			ctrl_seq = ctrl_seq[1:]
+# 			ctrl_seq.append(ctrl)
+# 			yield np.array(img_seq).reshape(1,seq_len,*im.shape), np.array(ctrl_seq).reshape(1,seq_len,2)
 
-		return imread(os.path.join(data_dir,im_name))
+def data_seq_from_json_list(json_list,data_dir):
+	img_seq = []
+	ctrl_seq = []
+	path,_ = os.path.split(data_dir)
+	for filename in json_list:
+		with open(os.path.join(path,filename)) as f:
+			df = json.load(f)
+		im   = get_img(data_dir,df["cam/image_array"])
+		ctrl = df["user/throttle"],df["user/angle"]
+		img_seq.append(im)
+		ctrl_seq.append(ctrl)
+	return np.array(img_seq), np.array(ctrl_seq)
+
+def data_generator(data_dir, seq_len=9):
+	path, basename = os.path.split(data_dir)
+	table = {
+		"json":[],
+		"y":[]
+	}
+	with open(os.path.join(path,basename+"_filter.csv")) as f:
+		f.readline()	#Skip the header
+		for line in f:
+			json_name, y = line.strip().split(",")
+			table["json"].append(json_name)
+			table["y"].append(int(y))
+
+	for i in range(seq_len-1,len(table["y"])):
+		X = data_seq_from_json_list(table["json"][i-seq_len+1:i+1],data_dir)
+		yield X,table["y"][i]
+
+
+def get_img(data_dir,im_name):
+#	with open(im) as f:
+#		im_name = json.load(f)["cam/image_array"]
+	return imread(os.path.join(data_dir,im_name))
+
 
 def build_model():
 	img_net = load_image_subnet()
@@ -77,4 +117,33 @@ def build_model():
 def make_plot(model):
 	from keras.utils import plot_model
 	plot_model(model,model.name+".png",show_shapes=True,show_dtype=True,show_layer_names=True,expand_nested=True,dpi=300)
+
+class DataGen(keras.utils.Sequence):
+	def __init__(self, df,X_col,y_col,batch_size):
+		self.batch_size = batch_size
+		self.df = df
+		self.X_col = X_col
+		self.y_col = y_col
+		pass
+
+	def __get_data(self,batches):
+
+		return X,y
+
+	def __getitem__(self, item):
+		'''
+		Assemble and load the next mini-batch
+		:param item: the item-th minibatch from the whole dataset
+		:return: a mini-batch
+		'''
+
+		batches = self.df[item*self.batch_size:(item+1)*self.batch_size]
+		X, y = self.__get_data(batches)
+		return  X, y
+
+	def on_epoch_end(self):
+		pass
+
+	def __len__(self):
+		pass
 
--- build_net_demo.py
+++ build_net_demo.py
@@ -10,14 +10,19 @@ files_list = get_img_jsonlist_ordered(TEST_DATA_DIR)
 
 x_bar = [x for x in range(128)]
 
-for seq in seq_generator(files_list):
-	output = model.predict(seq)
-	plt.subplot(2,1,1)
-	plt.imshow(np.concatenate((seq[0,0],seq[0,4],seq[0,8]),axis=1))
-	plt.subplot(2,1,2)
-	plt.bar(x_bar,output.flatten())
-	axes = plt.gca()
-	axes.set_ybound(-1,1)
-	axes.set_xbound(0,127)
-	plt.pause(0.0625)
-	plt.clf()
+# for seq in seq_generator(data_dir,files_list):
+# 	output = model.predict(seq)
+# 	plt.subplot(2,1,1)
+# 	plt.imshow(np.concatenate((seq[0,0],seq[0,4],seq[0,8]),axis=1))
+# 	plt.subplot(2,1,2)
+# 	plt.bar(x_bar,output.flatten())
+# 	axes = plt.gca()
+# 	axes.set_ybound(-1,1)
+# 	axes.set_xbound(0,127)
+# 	plt.pause(0.0625)
+# 	plt.clf()
+
+data_gen = data_generator(data_dir,files_list)
+model = build_model()
+model.compile(optimizer="rmsprop",loss="binary_crossentropy")
+model.fit_generator(data_gen)
